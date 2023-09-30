7895e817aa918af870984dc3de167844f59698d4
AnjaniTiwari <anjanitiwari001@gmail.com>
2023-09-30 16:38:26 +0530

    added some functions

--- tree/b_plus_tree.c
+++ tree/b_plus_tree.c
@@ -29,29 +29,65 @@ typedef struct {
 	data *next;
 }data_next;
 
+short tree_degree = 1;
 
 void create_bp_tree(bp_tree** tree) {
 	*tree = (bp_tree*)malloc(sizeof(bp_tree));
 	if(!(*tree)) {
-        printf("B+ tree memory allocation failed.\n");
+		printf("B+ tree memory allocation failed.\n");
 	}
+	(*tree)->d_array = NULL;
+	(*tree)->d_index = -1;
+}
+
+int d_array_grow(bp_tree** tree) {
+	 (*tree)->d_array = (data**)realloc((*tree)->d_array, (sizeof((*tree)->d_array) * (++(*tree)->d_index + 1)));
+	 if(!(*tree)->d_array) {
+		printf("get_data_memory allocation failed.\n");
+		return 1;
+	 }
+	 (*tree)->d_array[(*tree)->d_index] = NULL;
+	 return 0;
+}
+
+void create_id(bp_tree** tree, int id) {
+	(*tree)->d_array[(*tree)->d_index] = (data*)malloc(sizeof(data));
+	if(!(*tree)->d_array[(*tree)->d_index]) {
+		printf("create_id allocation failed.\n");
+		return;
+	}
+	(*tree)->d_array[(*tree)->d_index]->id = id;
+	(*tree)->d_array[(*tree)->d_index]->ptr = NULL;
 }
 
 void add_id(bp_tree** tree, int id) {
+	if(!d_array_grow(tree)) {
+		create_id(tree, id);
+	}
+}
+
+void split(bp_tree** tree) {
 
 }
 
 void insert(bp_tree** tree, int id) {
 	if(*tree == NULL)
 		create_bp_tree(tree);
-    add_id(tree, id);
+	add_id(tree, id);
+	if((*tree)->d_index == tree_degree-1)
+        split(tree);
 }
 
 int main() {
 	int id_array[] = {5, 10, 15, 25, 35, 40, 45, 50, 60};
 	bp_tree* tree = NULL;
+	int i, size = sizeof(id_array) / sizeof(id_array[0]);
+	for(i = 0; i < size; i++)
+		insert(&tree, id_array[i]);
 
-    insert(&tree, id_array[0]);
+	for(i = 0; i < size; i++)
+		printf("%d ", tree->d_array[i]->id);
 
+	getch();
 	return 0;
 }
