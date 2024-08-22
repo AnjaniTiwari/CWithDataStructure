dbf348b9582428745863f52b8147335990f9e516
AnjaniTiwari <anjanitiwari001@gmail.com>
2024-08-22 18:19:56 +0530

    delection bottom up approach logic.

--- tree/splay_tree/splay_tree.c
+++ tree/splay_tree/splay_tree.c
@@ -43,7 +43,7 @@ void insert_splay_node(splay_node** root, int data) {
 	else {
 		if(data < (*root)->data) {
 			insert_splay_node(&(*root)->left, data);
-            zig_rotation(root);
+			zig_rotation(root);
 		}
 		else if(data > (*root)->data) {
 			insert_splay_node(&(*root)->right, data);
@@ -53,13 +53,77 @@ void insert_splay_node(splay_node** root, int data) {
 	}
 }
 
+void inorder_predecessor(splay_node** root, int* data) {
+	if(!(*root)->right) {
+        splay_node* temp = NULL;
+		*data = (*root)->data;
+		if((*root)->left != NULL) {
+			temp = (*root)->left;
+		}
+		free(*root);
+		*root = temp;
+	}
+	else
+    	inorder_predecessor(&(*root)->right, data);
+}
+
+void inorder_successor(splay_node** root, int* data) {
+	if(!(*root)->left) {
+		splay_node* temp = NULL;
+		*data = (*root)->data;
+		if((*root)->right != NULL)
+			temp = (*root)->right;
+		free(*root);
+		*root = temp;
+	}
+	else
+		inorder_successor(&(*root)->left, data);
+}
+
+void delete_bottom_up_splay_node(splay_node** root, int data) {
+	if(data < (*root)->data) {
+		if((*root)->left != NULL)
+			delete_bottom_up_splay_node(&(*root)->left, data);
+		if((*root)->left != NULL)
+			zig_rotation(root);
+	}
+	else if(data > (*root)->data) {
+		if((*root)->right != NULL)
+			delete_bottom_up_splay_node(&(*root)->right, data);
+		if((*root)->right != NULL)
+			zag_rotation(root);
+	}
+	else {
+		if((*root)->left != NULL)
+			inorder_predecessor(&(*root)->left, &(*root)->data);
+		else if((*root)->right != NULL)
+			inorder_successor(&(*root)->right, &(*root)->data);
+		else {
+			free(*root);
+			*root = NULL;
+		}
+    }
+}
+
+void delete_bottom_up(splay_node** root, int data) {
+	if((*root) != NULL)
+		delete_bottom_up_splay_node(root, data);
+	else
+        printf("Empty tree.\n");
+}
+
 int main() {
 	int arr[] = {10, 7, 15, 1, 9, 13, 20, -1, 8, 11, 14, 30};
+	int arr2[] = {14, -1, 1, 11, 13, 10, 7, 9, 30, 20, 15, 8};
 	unsigned int arr_count = sizeof(arr)/sizeof(arr[0]);
 	splay_node* root = NULL;
 	int i;
+
 	for(i = 0; i < arr_count; i++)
 		insert_splay_node(&root, arr[i]);
+
+	for(i = 0; i < arr_count; i++)
+		delete_bottom_up(&root, arr2[i]);
 
 	getch();
 	return 0;
