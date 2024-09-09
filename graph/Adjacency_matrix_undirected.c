88c18745b07ee5852ce2fde9d213becbf58e4229
AnjaniTiwari <anjanitiwari001@gmail.com>
2024-09-09 20:07:04 +0530

    some small changes.

--- graph/Adjacency_matrix_undirected.c
+++ graph/Adjacency_matrix_undirected.c
@@ -13,12 +13,14 @@
 */
 
 
-void print_graph(int(*graph)[4], unsigned short row, unsigned short column) {
+void print_graph(int** graph, unsigned short row, unsigned short column) {
 	unsigned short i, j;
-	for(i = 0; i < row; ++i) {
-		for(j = 0; j < column; ++j)
-			printf("%d ", graph[i][j]);
-		printf("\n");
+	if(graph != NULL) {
+		for(i = 0; i < row; ++i) {
+			for(j = 0; j < column; ++j)
+				printf("%d ", graph[i][j]);
+			printf("\n");
+		}
 	}
 }
 
@@ -46,22 +48,28 @@ int** make_grid(unsigned int rows, unsigned int size) {
 
 void connect_vertices(int** const graph, unsigned int rows) {
 	unsigned int edge_count = 0, origin, desti, total_edge, total_vertices;
-	unsigned short run = 1;
-    total_vertices = rows*2;
+	unsigned short run = TRUE, flag = TRUE;
+	total_vertices = rows*rows;
 	total_edge = (total_vertices*(total_vertices-1))/2;
 	while(run && (edge_count < total_edge)) {
 		printf("Enter the two vertices between 1 and %u that you want to connect.\n"
 				,rows);
 		scanf("%u%u", &origin, &desti);
 		if((origin > 0 && origin <= rows) && (desti > 0 && desti <= rows)) {
-			graph[origin][desti] = 1;
-			graph[desti][origin] = 1;
+			graph[origin-1][desti-1] = 1;
+			graph[desti-1][origin-1] = 1;
+			flag = TRUE;
 		}
 		else {
 			printf("Invalid vertices (%u, %u)\n", origin, desti);
-			printf("Try again enter 1 for exit enter 0");
+			printf("Try again enter 1 for exit enter 0?\n");
 			scanf("%hu", &run);
+			flag = FALSE;
 		}
+		if(flag) {
+			printf("Continue press 1 exit press 0: ");
+			scanf("%hu", &run);
+        }
 	}
 }
 
@@ -71,12 +79,13 @@ int** create_graph(void) {
 
 	printf("Enter your graph grid total number of rows.\n");
 	scanf("%u", &rows);
-	if(rows == 0)
+	if(rows == 0 || rows == 1)
 		printf("Rows: %u invalid input.", rows);
-	graph = make_grid(rows, sizeof(int));
+	else
+		graph = make_grid(rows, sizeof(int));
 	if(graph != NULL)
 		connect_vertices(graph, rows);
-    return graph;
+	return graph;
 }
 
 int main() {
@@ -84,7 +93,8 @@ int main() {
 
 	graph = create_graph();
 
+	print_graph(graph, 4, 4);
 
-	getchar();
+	getch();
 	return 0;
 }
