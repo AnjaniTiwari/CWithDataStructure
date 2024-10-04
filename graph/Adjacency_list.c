da716e865e51ab818d3291d97773aaeef399a9fb
AnjaniTiwari <anjanitiwari001@gmail.com>
2024-10-04 19:37:08 +0530

    depth first search logic implemented and working on bfs logic.

--- graph/Adjacency_list.c
+++ graph/Adjacency_list.c
@@ -12,6 +12,21 @@ struct vertex {
 	vertex* next_vertex;
 };
 
+typedef struct {
+	int* vertices;
+	unsigned int size;
+}visited;
+
+typedef struct {
+	vertex* arr;
+	unsigned int size;
+}queue;
+
+visited visit_var;
+unsigned int index;
+
+queue vertex_queue;
+
 vertex* get_vertex(vertex* list, unsigned short vertex_data) {
 	do{
 		if(list->data == vertex_data)
@@ -76,16 +91,20 @@ void connect_edge(vertex* new_vertex, vertex** list) {
 		if(remain_vertex) {
 			printf("\nEnter vertex to make edge: ");
 			scanf("%d", &vertex_data);
-			pos_ptr = get_vertex(*list, vertex_data);
-			if(pos_ptr) {
-				if(grow_edge_arr(&new_vertex)) {
-					new_vertex->edge_arr[new_vertex->edge_count-1] = pos_ptr;
-					printf("Point %d has been connected to point %d successfully.\n",
-							new_vertex->data, vertex_data);
+			if(vertex_data != new_vertex->data) {
+				pos_ptr = get_vertex(*list, vertex_data);
+				if(pos_ptr) {
+					if(grow_edge_arr(&new_vertex)) {
+						new_vertex->edge_arr[new_vertex->edge_count-1] = pos_ptr;
+						printf("Point %d has been connected to point %d successfully.\n",
+								new_vertex->data, vertex_data);
+					}
 				}
+				else
+					printf("%d vertex does not exist in the graph.\n", vertex_data);
 			}
 			else
-				printf("%d vertex does not exist in the graph.\n", vertex_data);
+            	printf("Invalid input.\n");
 			printf("If you want to continue then press 1, to exit press 0.\n");
 			scanf("%hu", &run);
 		}
@@ -141,9 +160,9 @@ void add_vertex(vertex** list) {
 void show_graph(vertex* list) {
 	unsigned short i;
 	while(list) {
-		printf("Vertex: %4d Edges: ", list->data);
+		printf("Vertex: %2d Edges: ", list->data);
 		for(i = 0; i < list->edge_count; ++i) {
-			printf("%d", list->edge_arr[i]->data);
+			printf("%d ", list->edge_arr[i]->data);
 		}
 		printf("\n");
         list = list->next_vertex;
@@ -245,15 +264,6 @@ void delete_vertex(vertex** list) {
 	}
 }
 
-void show_menu(void) {
-	printf("\n1.Add vertex.\n");
-	printf("2.Add edge.\n");
-	printf("3.Delete vertex.\n");
-	printf("4.Delete edge.\n");
-	printf("5.Show graph.\n");
-	printf("6.Exit.\n");
-}
-
 void free_list(vertex** list) {
     vertex* next = NULL;
 	do {
@@ -289,16 +299,132 @@ void delete_edge(vertex** list)  {
 					   printf("%d is not a edge.\n", edge);
 				}
 				else
-                	printf("%d vertex not have any edge.\n", select_vertex->data);
+					printf("%d vertex not have any edge.\n", select_vertex->data);
 				printf("Enter 1 to continue or 0 to exit.");
-            }
+			}
 			else {
-				printf("%d invalid vertex not find in graph.\n");
+				printf("%d invalid vertex not find in graph.\n", vertex_data);
 				printf("try again press 1 or press exit 0.\n");
 			}
 			scanf("%d", &run);
 		}
 	}
+}
+
+short visit(int data) {
+	unsigned int size = visit_var.size;
+	unsigned int i;
+
+	if(visit_var.vertices == NULL || index == visit_var.size) {
+		size = size == 0?10:size*2;
+		visit_var.vertices = (int*)realloc(visit_var.vertices, sizeof(int)*size);
+		if(!visit_var.vertices) {
+			printf("size %d memory allocation falied.\n", visit_var.size);
+			return 0;
+		}
+		visit_var.size = size;
+	}
+
+	for(i = 0; i < index; ++i) {
+		if(visit_var.vertices[i] == data)
+			return FALSE;
+	}
+	printf("%d ", data);
+	visit_var.vertices[index++] = data;
+	return TRUE;
+}
+
+void depth_first_search(vertex* start) {
+	unsigned short i;
+	for(i = 0; i < start->edge_count; ++i) {
+		if(visit(start->edge_arr[i]->data))
+			depth_first_search(start->edge_arr[i]);
+	}
+}
+
+void dfs_vertex_input(vertex* list)  {
+	int vertex_data;
+	unsigned short run = TRUE;
+	vertex* start_vertex = NULL;
+
+	if(list ==   NULL) {
+		printf("Graph is empty.\n");
+	}
+	else {
+		while(run) {
+			show_graph(list);
+			printf("Enter the vertex from where you want to start dfs.\n");
+			scanf("%d", &vertex_data);
+			start_vertex = get_vertex(list, vertex_data);
+			if(start_vertex != NULL) {
+			   printf("Depth first search output.\n");
+			   visit(start_vertex->data);
+			   depth_first_search(start_vertex);
+			   printf("\nEnter 1 to continue or 0 to exit.\n");
+			}
+			else {
+				printf("%d invalid vertex not find in graph.\n", vertex_data);
+				printf("try again press 1 or press exit 0.\n");
+			}
+			scanf("%d", &run);
+			if(visit_var.vertices != NULL) {
+				free(visit_var.vertices);
+				visit_var.vertices = NULL;
+			}
+			visit_var.size = 0;
+			index = 0;
+		}
+	}
+}
+
+
+void breadth_first_search(vertex* start_vertex) {
+	if(vertex_queue.arr == NULL || )
+}
+
+void bfs_vertex_input(vertex* list)  {
+	int vertex_data;
+	unsigned short run = TRUE;
+	vertex* start_vertex = NULL;
+
+	if(list ==   NULL) {
+		printf("Graph is empty.\n");
+	}
+	else {
+		while(run) {
+			show_graph(list);
+			printf("Enter the vertex from where you want to start dfs.\n");
+			scanf("%d", &vertex_data);
+			start_vertex = get_vertex(list, vertex_data);
+			if(start_vertex != NULL) {
+			   printf("Breadth first search output.\n");
+			   breath_first_search(start_vertex);
+			   printf("\nEnter 1 to continue or 0 to exit.\n");
+			}
+			else {
+				printf("%d invalid vertex not find in graph.\n", vertex_data);
+				printf("try again press 1 or press exit 0.\n");
+			}
+			scanf("%d", &run);
+			if(visit_var.vertices != NULL) {
+				free(visit_var.vertices);
+				visit_var.vertices = NULL;
+			}
+			visit_var.size = 0;
+			index = 0;
+		}
+	}
+}
+
+void show_menu(void) {
+	printf("\n1.Add vertex.\n");
+	printf("2.Add edge.\n");
+	printf("3.Delete vertex.\n");
+	printf("4.Delete edge.\n");
+	printf("5.Show graph.\n");
+	printf("6.Depth first search.\n");
+	printf("7.Depth first search.\n");
+	printf("8.Exit.\n");
 }
 
 int main() {
@@ -321,12 +447,17 @@ int main() {
                 	break;
 			case 5: show_graph(list);
 					break;
-			case 6: run = FALSE;
+			case 6: dfs_vertex_input(list);
+					break;
+			case 7: bfs_vertex_input(list);
+					break;
+			case 8: run = FALSE;
 					break;
 			default:
 					printf("%d invaild choice.\n", choice);
 		}
 	}
+
 	if(list != NULL)
     	free_list(&list);
 	return 0;
